94baa79564aeba258a9b4bca6ad45f9c36090ab4
Daniil <sitnikovdan13@gmail.com>
2020-09-22 23:02:08 +0300

    init: vigenere-cipher

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,26 +1,34 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
-  let res = [];
+    if (typeof arr !== 'object') {
+        throw new Error('Error')
+    }
+    let res = [];
     for (let i = 0; i < arr.length; i++) {
         if (arr[i - 1] == '--discard-next') {
-            res.pop();
+
         } else if (arr[i] == '--discard-prev' && arr[0] != '--discard-prev') {
             res.pop()
         } else if (arr[i] == '--double-next') {
             res.push(arr[i + 1])
         } else if (arr[i] == '--double-prev') {
-            res.push(arr[i - 1])
-        } else if (arr[i] != '--discard-next' && arr[i] != '--discard-next') {
+            if (arr[i - 2] == '--discard-next') {
+                res.push(arr[i - 3])
+            } else {
+                res.push(arr[i - 1])
+            }
+        } else if (arr[i] != '--discard-next' && arr[i] != '--discard-prev' && arr[i - 1] != '--discard-next') {
             res.push(arr[i]);
         }
     }
+
     if (res[0] == '--discard-prev' || res[0] == undefined) {
-      res.shift();
+        res.shift();
     }
 
     if (res[res.length - 1] == undefined) {
-      res.pop();
+        res.pop();
     }
     return res;
 };
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,123 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    constructor() {
+        this.arr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    }
+    check(message, key) {
+        if (!message || !key) {
+            throw new Error('Error')
+        }
+    }
+    encrypt(message, key) {
+        let preRes = [],
+            res = [],
+            copyMes = message.split(' '),
+            finish = [];
+
+        this.check(message, key);
+        message = message.toUpperCase().split(' ').join('').split('');
+        key = key.toUpperCase().split('');
+
+        for (let i = 0; i < key.length; i++) {
+            if (message.length > key.length) {
+                key.push(key[i]);
+            }
+        }
+
+        for (let i = 0; i < message.length; i++) {
+            if (this.arr.includes(message[i]) && this.arr.includes(key[i])) {
+                let addIndex = this.arr.indexOf(message[i]) + this.arr.indexOf(key[i]);
+                if (addIndex >= 26) {
+                    addIndex = addIndex - 26;
+                }
+                preRes.push(addIndex);
+            } else {
+                preRes.push(message[i]);
+            }
+        }
+
+        for (let i = 0; i < preRes.length; i++) {
+            if (typeof preRes[i] == 'number') {
+                for (let j = 0; j < this.arr.length; j++) {
+                    if (this.arr.indexOf(this.arr[j]) === preRes[i]) {
+                        res.push(this.arr[j]);
+                    }
+                }
+            } else {
+                res.push(preRes[i]);
+            }
+        }
+
+        let checker = 0;
+
+        for (let i = 0; i < copyMes.length; i++) {
+            for (let j = 0; j < copyMes[i].length; j++) {
+                finish.push(res[checker]);
+                checker += 1;
+            }
+            finish.push(' ');
+        }
+
+        finish.pop();
+
+        return finish.join('');
+    }
+
+    decrypt(message, key) {
+        let preRes = [],
+            res = [],
+            copyMes = message.split(' '),
+            finish = [];
+
+        this.check(message, key);
+        message = message.toUpperCase().split(' ').join('').split('');
+        key = key.toUpperCase().split('');
+
+        for (let i = 0; i < key.length; i++) {
+            if (message.length > key.length) {
+                key.push(key[i]);
+            }
+        }
+
+        for (let i = 0; i < message.length; i++) {
+            if (this.arr.includes(message[i]) && this.arr.includes(key[i])) {
+                let addIndex = this.arr.indexOf(message[i]) - this.arr.indexOf(key[i]);
+                if (addIndex < 0) {
+                    addIndex = addIndex + 26;
+                }
+                preRes.push(addIndex);
+            } else {
+                preRes.push(message[i]);
+            }
+        }
+
+        for (let i = 0; i < preRes.length; i++) {
+            if (typeof preRes[i] == 'number') {
+                for (let j = 0; j < this.arr.length; j++) {
+                    if (this.arr.indexOf(this.arr[j]) === preRes[i]) {
+                        res.push(this.arr[j]);
+                    }
+                }
+            } else {
+                res.push(preRes[i]);
+            }
+        }
+
+        let checker = 0;
+
+        for (let i = 0; i < copyMes.length; i++) {
+            for (let j = 0; j < copyMes[i].length; j++) {
+                finish.push(res[checker]);
+                checker += 1;
+            }
+            finish.push(' ');
+        }
+
+        finish.pop();
+
+        return finish.join('');
+    }
 }
 
 module.exports = VigenereCipheringMachine;
